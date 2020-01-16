edf79f6962e30e0ed77096f8ea30915a37f1910c
Celso Alexandre <celsoalexandre@live.com>
2020-01-16 00:34:06 -0300

    Created the backend with node and web react project folder with create-react-app script. Made a single route to register new users, integrated with the backend project.

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,34 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+  async index(req, res) {
+    const { latitude, longitude, techs } = req.query
+    const techsArray = parseStringAsArray(techs)
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    })
+
+    return res.json(devs)
+  }
+  ,
+  async update (req, res) {
+    //todo
+  }
+  ,
+  async destroy (req, res) {
+    //todo
+  }
+}
